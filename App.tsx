c63a8ef7828fc717b062abc22c88b6927475046c
Radiant-F <radiantfadilah0@gmail.com>
2022-04-16 10:57:45 +0700

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SetStateAction, useEffect, useState } from "react";
+import { Button, Text } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StreamChat } from "stream-chat";
+import {
+  OverlayProvider,
+  Chat,
+  ChannelList,
+  Channel,
+  MessageList,
+  MessageInput,
+} from "stream-chat-expo";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+const API_KEY = "dwjb8apx9kx3";
+const client = StreamChat.getInstance(API_KEY);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const [selectedCh, setSelectedCh] = useState<any>(null);
+
+  useEffect(() => {
+    const connectUser = async () => {
+      try {
+        await client.connectUser(
+          {
+            id: "radiant",
+            name: "Radiant Fadilah",
+            image:
+              "https://res.cloudinary.com/dbdih4uiv/image/upload/v1648174433/ximzrkcmow9obrhnjb2l.jpg",
+          },
+          client.devToken("radiant")
+        );
+
+        // creating a channel
+        const channel = client.channel("messaging", "unichat", {
+          name: "UniChat Channel",
+        });
+        await channel.watch();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    connectUser();
+
+    return () => client.disconnectUser();
+  }, []);
+
+  const selectChannel = (channel) => {
+    console.log(channel);
+    setSelectedCh(channel);
+  };
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <OverlayProvider>
+          <Chat client={client}>
+            {selectedCh ? (
+              <Channel channel={selectedCh}>
+                <MessageList />
+                <MessageInput />
+                <Text>channel selected: {selectedCh}</Text>
+              </Channel>
+            ) : (
+              <ChannelList onSelect={selectChannel} />
+            )}
+            {/* <Navigation colorScheme={colorScheme} /> */}
+          </Chat>
+        </OverlayProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
